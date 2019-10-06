18706b22ced23f2c0e4fe62efca91e9e4dfa480e
Kartika Chauhan <49581062+kartikachauhan@users.noreply.github.com>
2019-10-06 08:53:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $("#search-box").keyup(function(event){
+        var x = event.keyCode;
+        if(x == 13){
+            $(".search").css("top","25%");
+            $(".search").css("transform","translate(-50%, 0%)");
+            setTimeout(function(){
+                $(".results-based-on-search").css("display", "block");
+            }, 800);
+        }
+        var value = $('#search-box').val();
+            console.log('inside');
+            var repositoryURL = 'https://api.github.com/users/'+value+'/repos';
+
+            $.ajax({
+                type: 'GET',
+                url: repositoryURL,
+                success: function(data) {
+                    createCard(data);
+                },
+            });
+
+
+        if ( value.length > 0) {
+            $("button").removeClass("disable");
+            $("button").addClass("working");
+            $('#clearSearchButton').css('display', 'block'); 
+            $(".search__button").click(function(){
+                $(".search").css("top","25%");
+                $(".search").css("transform","translate(-50%, 0%)");
+                setTimeout(function(){
+                    $(".results-based-on-search").css("display", "block");
+                }, 800);
+            });
+        }else{
+            $("button").addClass("disable");
+            $("button").removeClass("working");
+            $('#clearSearchButton').css('display', 'none');
+        }
+
+    });
+
+
+    $("#clearSearchButton img").click(function(){
+        var searchbox  =  $("#search-box");
+        searchbox.val("");
+        $("#clearSearchButton").css("display", "none");
+        setTimeout(function(){
+            $(".results-based-on-search").css("display", "none");
+            $(".search").css("top","50%");
+            $(".search").css("transform","translate(-50%, -50%)");
+        }, 300);
+    });
+
+    var cardtext = "";
+    
+    function createCard(data){
+        console.log(data);
+        for(key in data){
+                cardtext += `<div class="fold-oneBytwo--card fold-oneBytwo--card-one">
+                                <a href="${data[key].url}" target="_blank">
+                                    <div class="fold-card--details">
+                                        <div class="fold-card--details-header">${data[key].name}</div>`;
+                                        if(data[key].description == null){
+                                            cardtext += `<div class="fold-card--details-text">No Description Mentioned</div>`;
+                                        }else{
+                                            cardtext += `<div class="fold-card--details-text">${data[key].description}</div>`;
+                                        }
+                                        cardtext += `<div class="fold-card--details-support-text">Last Updated: <span class="fold-card--details-support-text">${localTime(data[key].updated_at)}</span></div>
+                                    </div>
+                                </a>
+                            </div>`;
+        }
+        $(".fold-oneBytwo-container").html(cardtext);
+    }
+
+    function localTime(dteTime){
+        var date1 = new Date(dteTime);
+        return date1;
+    }
+});
